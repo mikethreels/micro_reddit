218637d9b5e8e9ed35672a6f2e9b2cf1c0b6e123
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-07-10 17:40:54 -0500

    Added model comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
 
   validates :title, presence: {message: 'Title post cannot be empty'}
   validates :body, presence: {message: 'Post must have content'}
---
+++ db/migrate/20200710223731_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.string :title
+      t.text :body
+      t.references :posts, foreign_key: true
+      t.references :users, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
