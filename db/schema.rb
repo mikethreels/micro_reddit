1c0173fdcbc893e4874c7bd1a8e52e9aaecf9304
mikethreels <michaelthreels@gmail.com>
2020-07-13 21:47:53 +0200

    fixed comment database issues

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,12 @@ ActiveRecord::Schema.define(version: 2020_07_10_223731) do
   create_table "comments", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "posts_id"
-    t.integer "users_id"
+    t.integer "post_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["posts_id"], name: "index_comments_on_posts_id"
-    t.index ["users_id"], name: "index_comments_on_users_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
